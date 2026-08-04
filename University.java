aed381828d72d833a43b6235e1266637704a5fdb
Manohar <manohar.x03@gmail.com>
2026-08-04 08:01:42 +0530

    Taking inputs from user

--- University.java
+++ University.java
@@ -16,15 +16,29 @@ public class University {
         students.add(student);
     }
 
-    public void addStudentCourse(int id, Course course) {
+    public void addStudentCourse(int id, int courseId) {
+
+        Course course = null;
 
         for(Student student : students) {
 
             if(student.getStudentId() == id) {
+                course = findCourse(courseId);
                 student.addCourse(course);
                 break;
             }
         }
+    }
+
+    public Course findCourse(int courseId) {
+
+        for(Course course : courses) {
+
+            if(course.getCourseId() == courseId) {
+                return course;
+            }
+        }
+        return null;
     }
 
     public void registerCourse(Course course) {
